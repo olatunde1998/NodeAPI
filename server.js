c5dd27c439ed4462466783ccc0939475bbf7b5db
geodevcode <geodevinstitute@gmail.com>
2023-11-04 11:13:56 +0100

    server.js updated to standard way

--- server.js
+++ server.js
@@ -1,103 +1,52 @@
+require("dotenv").config();
 const express = require("express");
-const Product = require("./models/productModel");
 const mongoose = require("mongoose");
+const productRoute = require("./routes/productRoute");
+const errorMiddleware = require("./middleware/errorMiddleware");
+const cors = require("cors");
+
 const app = express();
 
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000;
+const FRONTEND = process.env.FRONTEND;
+
+var corsOptions = {
+  // origin: 'http://localhost:3000',
+  // origin: FRONTEND,
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "https://travel-manager-app-jade.vercel.app",
+    FRONTEND,
+  ], // this is for multiple url you wish
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
 //routes
+
+app.use("/api/products", productRoute);
 app.get("/", (req, res) => {
-  res.send("Hello world, thank you Rasheed");
+  res.send("Hello world, thank you geodevcodes");
 });
 app.get("/blog", (req, res) => {
   res.send("Hello world, thank you Rasheed blog route");
 });
 
-
-// create product
-app.post("/product", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//fetch all product
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//fetch a single product
-app.get("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Update a product
-app.put("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    //we cannot find any product in database
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
-//Delete a product
-app.delete("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
-
+app.use(errorMiddleware);
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://geodevcode:geodev0987654321@geodevapi.5cwlvpl.mongodb.net/NODE-API?retryWrites=true&w=majority"
-  )
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB!");
-    app.listen(3000, () => {
-      console.log("Node API app is running on Port 3000");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on Port ${PORT}`);
     });
   })
   .catch((error) => {
